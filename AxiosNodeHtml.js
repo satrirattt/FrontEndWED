27873b9675fffe3acdfd5a3b4255a2429c3eb2e4
satrirattt <s6606021620198@email.kmutnb.ac.th>
2025-02-19 14:22:16 +0700

    commit

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -5,12 +5,14 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require("path");
 
 //Base URL for the API
 //const base_url = "https://api.example.com";
 const base_url = "http://localhost:5000";
 
 //Set the template engine
+app.set("views" , path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
